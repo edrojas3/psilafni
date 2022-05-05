1e66417508661cbc3af2963dc2660fda0863af7b
edrojas3 <edrojas3@gmail.com>
2022-05-05 14:32:01 -0500

    psilafni_proc for standard use and for ucdavis

--- code/psilafni_animal_warper.sh
+++ code/psilafni_animal_warper.sh
@@ -9,23 +9,17 @@ site=$1
 subj=$2
 
 # DEFAULTS
-
 outdir=$site/data_aw/$subj
 container=/misc/purcell/alfonso/tmp/container/afni.sif
+
+# Get anatomical volume(s)
+subj_anat=$(find $site/$subj -type f -name "${subj}*T1*nii*")
 
 # Standard volume location
 refdir=/misc/hahn2/reduardo/atlases_and_templates/NMT_v2.0_sym/NMT_v2.0_sym_05mm
 refvol=$refdir/NMT*_SS.nii.gz
 refmask=$refdir/NMT*_brainmask.nii.gz
 refmask_ab=MASK
-
-# copy wm and ventricle mask
-cp $refdir/NMT_WM.nii.gz $outdir
-cp $refdir/NMT_VENT.nii.gz $outdir
-
-# Get anatomical volume(s)
-subj_anat=$(find $site/$subj -type f -name "${subj}*T1*nii*")
-
 
 # @animal_warper.py
 singularity exec -B /misc:/misc --cleanenv $container @animal_warper \
